810602e8cc3793d6c7f6286636611d98183bcbb3
Kolen Cheung <christian.kolen@gmail.com>
2016-11-20 20:12:12 -0800

    mathjax setup: vanilla js version & cleanup

---
+++ js/mathjax/setup-mathjax-cdn.js
@@ -0,0 +1,6 @@
+window.MathJax = {
+  TeX: {
+    equationNumbers: { autoNumber: "AMS" },
+    extensions: ["[MathJax]/extensions/TeX/cancel.js","[Contrib]/physics/physics.js","[Contrib]/siunitx/siunitx.js"]
+  }
+};
