bc42188b8a0d2920856a676c6d69ffc86d763064
Kolen Cheung <christian.kolen@gmail.com>
2016-11-27 01:24:09 -0800

    setup-mathjax-cdn: html use javascript; simplified

--- js/mathjax/setup-mathjax-cdn.js
+++ js/mathjax/setup-mathjax-cdn.js
@@ -1,6 +1,6 @@
 window.MathJax = {
   TeX: {
     equationNumbers: { autoNumber: "AMS" },
-    extensions: ["[MathJax]/extensions/TeX/cancel.js","[Contrib]/physics/physics.js","[Contrib]/siunitx/siunitx.js"]
+    extensions: ["cancel.js","[Contrib]/physics/physics.js","[Contrib]/siunitx/siunitx.js"]
   }
 };
