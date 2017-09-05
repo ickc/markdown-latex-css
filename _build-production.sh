be8bb4e5729d35e54b8af1c6a3e94b862b04078a
Kolen Cheung <christian.kolen@gmail.com>
2017-09-04 18:48:46 -0700

    remove useless files

--- _build-production.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-python -mwebbrowser http://127.0.0.1:4000/markdown-latex-css/
-JEKYLL_ENV=production bundle exec jekyll serve --profile --drafts --incremental
